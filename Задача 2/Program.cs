95670b6611be0d3a3097d26839b0cc8103cab400
canya <canya@DESKTOP-0RBMO97>
2021-12-27 19:54:40 +0300

    Добавьте файлы проекта.

---
+++ Задача 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ввести с клавиатуры предложение. 
+              Предложение представляет собой слова, разделенные пробелом. Знаки препинания не используются.
+              Составить программу, определяющую является ли строка палиндромом без учёта пробелов и регистра 
+              (пример палиндрома – «А роза упала на лапу Азора»). */
+
+            Console.Write("Введите предложение: ");
+            string stroka = Console.ReadLine();
+            string[] array = stroka.Split(' ');
+            string rez = string.Join("", array);
+            rez = rez.ToUpper();
+            char[] inputarray = rez.ToCharArray();
+            Array.Reverse(inputarray);
+            string output = new string(inputarray);
+            output = output.ToUpper();
+            Console.WriteLine();
+            if (output==rez)
+            {
+                Console.WriteLine("Строка является палиндромом!");
+            }
+            else
+            {
+                Console.WriteLine("Строка не является палиндромом!");
+            }
+            
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Задача_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ввести с клавиатуры предложение. 
+                 Предложение представляет собой слова, разделенные пробелом.Знаки препинания не используются. 
+                 Найти самое длинное слово в строке. */
+
+            Console.Write("Введите предложение: ");
+            string stroka = Console.ReadLine();
+            string[] array = stroka.Split(' ');
+            int length = array.Length;
+            Console.WriteLine();
+            int sMax = array[0].Length;
+            string slovoMax = "";
+            foreach (var a in array)
+            {
+                Console.WriteLine("{0,-15} - {1,5}", a, a.Length);
+                if (a.Length > sMax)
+                {
+                    sMax = a.Length;
+                    slovoMax = a;
+                }
+            }
+            Console.WriteLine();
+          
+            Console.WriteLine("Самое длинное слово: {0} из {1} букв(ы)", slovoMax, sMax);
+            Console.ReadKey();
+        }
+    }
+}
